cd3e1ac041b959dfc6c8a83a762614d2e2e4e4e8
AndrieiGensh <64590764+AndrieiGensh@users.noreply.github.com>
2020-06-06 00:44:19 +0200

    Added Transmitter.hpp file

---
+++ host/include/Transmitter.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include "Codec.hpp"
+#include "msgqueue.hpp"
+#include <atomic>
+
+// a transmitter class. 
+
+// considering the fact, that the loop in the member thread if infinite,
+// we should have the way to stop it, otherwise, even if 
+// the .join() function is called, the thread wont finish.
+
+class Transmitter {
+
+	// transmitter thread-member
+	std::thread transmitter_thread;
+
+	// pointer to a message queue
+	MsgQueue *toSendQueue;
+
+	// thread function itself
+	void send();
+
+	// serial port file descriptor
+	int serial_port_fd;
+
+	// an atomic boolean variable, that has to be passed using std::ref 
+	// to the class constructor from the main function in order to control the 
+	// workflow of the thread
+	std::atomic<bool> program_status;
+
+public:
+	// param. constructor
+	Transmitter(std::atomic<bool>&, MsgQueue &);
+
+	// destructor
+	~Transmitter();
+};
